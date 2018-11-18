bdde584852acdf0fac602ecbc10421b420854352
徐沁心 <xuqinxin@er-10-131-66-53.wireless.umn.edu>
2018-11-18 13:10:31 -0600

    add post adapter and link home fragment with layout

--- Cultural/app/src/main/java/com/cultural/cultural/Post.java
+++ Cultural/app/src/main/java/com/cultural/cultural/Post.java
@@ -6,52 +6,60 @@ import java.util.List;
 
 public class Post {
     private int post_id;
-    private int user_id;
+    //private int user_id;
     private String title;
-    private List<String> tags;
+    private String tags;
+    private String country;
 
-    private String answer;
-    private List image;//?
-    private int video;
+    //private String answer;
+    private int image;//?
+    //private int video;
 
-    public Post(int post_id, int user_id, String title, List<String> tags, @Nullable String answer, @Nullable List image, @Nullable int video ){
+    public Post(int post_id, String title, String tags, String country, @Nullable int image){
         this.post_id = post_id;
-        this.user_id = user_id;
+        //this.user_id = user_id;
         this.title = title;
         this.tags = tags;
+        this.country = country;
 
-        this.answer = answer;
+        //this.answer = answer;
         this.image = image;
-        this.video = video;
+        //this.video = video;
     }
 
     public int get_post_id(){
         return post_id;
     }
 
-    public int get_user_id(){
-        return user_id;
+    /*
+    public int get_user_id(){return user_id;
     }
+     */
+
 
     public String get_title(){
         return title;
     }
 
-    public List<String> get_tags(){
+    public String get_tags(){
         return tags;
     }
 
-    public String get_answer() {
-        return answer;
+    public String get_country(){
+        return country;
     }
 
-    public List get_image() {//?
+    /*public String get_answer() {
+        return answer;
+    }*/
+
+    public int get_image() {//?
         return image;
     }
 
-    public int get_video(){
+    /*public int get_video(){
         return video;
-    }
+    }*/
 }
 
 
